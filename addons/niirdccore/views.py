ce3778cfde891fa0da6fec2d865e974548223c48
ivis-kuwata <jumpei.kuwata@ivis.co.jp>
2021-01-08 08:23:49 +0900

    (7) DMP送信APIを実装

--- addons/niirdccore/views.py
+++ addons/niirdccore/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from rest_framework import status as http_status
 from flask import request
+from django.db.models import Subquery
 import logging
 import requests
 
+from osf.models.node import Node
 from . import SHORT_NAME
 from . import settings
 from framework.exceptions import HTTPError
@@ -13,6 +15,9 @@ from website.project.decorators import (
     must_have_addon,
 )
 from addons.jupyterhub.apps import JupyterhubAddonAppConfig
+from addons.niirdccore.models import AddonList
+
+from addons import *
 
 logger = logging.getLogger(__name__)
 
@@ -61,3 +66,39 @@ def niirdccore_get_dmp_info(**kwargs):
     return {'data': {'id': node._id, 'type': 'dmp-status',
                     'attributes': dmp_info.text}}
 
+@must_be_valid_project
+@must_have_permission('admin')
+@must_have_addon(SHORT_NAME, 'node')
+def niirdccore_apply_dmp_subscribe(**kwargs):
+    node = kwargs['node']
+    addon_list = AddonList()
+
+    addon_list.set_node_title(node.title)
+    addon_list.set_addon_id(kwargs['addon_id'])
+    addon_list.set_callback(kwargs['callback'])
+    addon_list.set_owner(node.get_addon(SHORT_NAME))
+
+    return
+
+def niirdccore_dmp_notification(**kwargs):
+
+    # コールバック関数を呼び出す関数
+    def _notification_handler(func, **kwargs):
+        return func(**kwargs)
+
+    # リクエストボディ取得
+    try:
+        dmp_record = request.json['dmp']
+    except KeyError:
+        raise HTTPError(http_status.HTTP_400_BAD_REQUEST)
+
+    addon_list = AddonList.objects.all()
+
+    for addon in addon_list:
+        # デコレータ対策のため、nodeも引数に含める
+        _notification_handler(
+            func=eval(addon.callback),
+            node=Node.objects.get(title=addon.node_title),
+            dmp_record=dmp_record)
+
+    return
